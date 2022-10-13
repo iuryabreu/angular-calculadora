d497998aada8f5a7c0a89641d54898915cc91c90
Iury Abreu <iury.abreu@delage.com.br>
2022-10-13 12:10:24 -0300

    Implantando teste unitario da função de soma

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
 import {CalculadoraService} from '../services'
+import { By } from '@angular/platform-browser';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -24,4 +25,26 @@ describe('CalculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que a soma de entre 3 e 2 seja 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'))
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'))
+    let btn2 = fixture.debugElement.query(By.css('#btn2'))
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    btn3.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btnSoma.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btn2.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btnCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    expect(display.nativeElement.value).toEqual('5')
+  })
 });
