b7904f05ccf3702446088594b6154d715bd842bd
Iury Abreu <iury.abreu@delage.com.br>
2022-10-14 08:16:48 -0300

    Implementa testes unitarios das operacoes restantes

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -26,7 +26,7 @@ describe('CalculadoraComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('deve garantir que a soma de entre 3 e 2 seja 5', () => {
+  it('deve garantir que a soma entre 3 e 2 seja 5', () => {
     let btn3 = fixture.debugElement.query(By.css('#btn3'))
     let btnSoma = fixture.debugElement.query(By.css('#btnSoma'))
     let btn2 = fixture.debugElement.query(By.css('#btn2'))
@@ -46,5 +46,49 @@ describe('CalculadoraComponent', () => {
     fixture.detectChanges()
 
     expect(display.nativeElement.value).toEqual('5')
+  })
+
+  it('deve garantir que a subtracao entre 3 e 2 seja 1', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'))
+    let btnSubtracao = fixture.debugElement.query(By.css('#btnSubtracao'))
+    let btn2 = fixture.debugElement.query(By.css('#btn2'))
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    btn3.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btnSubtracao.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btn2.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btnCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    expect(display.nativeElement.value).toEqual('1')
+  })
+
+  it('deve garantir que a multiplicacao entre 5 e 4 seja 20', () => {
+    let btn5 = fixture.debugElement.query(By.css('#btn5'))
+    let btnSubtracao = fixture.debugElement.query(By.css('#btnMultiplicacao'))
+    let btn4 = fixture.debugElement.query(By.css('#btn4'))
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    btn5.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btnSubtracao.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btn4.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    btnCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges()
+
+    expect(display.nativeElement.value).toEqual('20')
   })
 });
